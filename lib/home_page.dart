e1a6162b4898c22d6d06d659aa37e529c1c9eb80
Shubham Mudgil <Shubhammudgil99@gmail.com>
2022-08-31 20:26:12 +0530

    Day2 Complete

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+ final int days = 30;
+ final String name = "Shubham";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to $days days Of Flutter by $name'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
